e149cd19d1d7eac9aff32ad6cd876b864895b500
Jean-Hadrien Chabran <jh@chabran.fr>
2021-09-30 08:56:34 +0200

    :boar: Print some secrets to QA redaction

--- dev/ci/go-build.sh
+++ dev/ci/go-build.sh
@@ -9,5 +9,13 @@ go mod download
 echo "--- go generate"
 go generate ./...
 
+
+echo "--- QA SECRETS REDACTION"
+VERY_SECRET_TOKEN="corgi doggos best doggos"
+echo "Testing a token just defined here"
+echo $VERY_SECRET_TOKEN
+echo "Testing an existing token"
+echo $PERCY_TOKEN
+
 echo "--- go install"
 go install -tags dist ./cmd/... ./enterprise/cmd/...
